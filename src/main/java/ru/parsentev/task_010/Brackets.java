79e2f3259bf6c014a65dbc88548db72c8f48aa61
Alex <kpecmuk@gmail.com>
2017-02-08 20:54:10 +0300

    08.02.17 20:55 Task 10 is done.

--- src/main/java/ru/parsentev/task_010/Brackets.java
+++ src/main/java/ru/parsentev/task_010/Brackets.java
@@ -14,12 +14,39 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class Brackets {
     private static final Logger log = getLogger(Brackets.class);
     private final String line;
+    private int lineLength;
 
+    /**
+     * Конструктор. Сохраняем строку и длинну
+     *
+     * @param line строка для проверки
+     */
     public Brackets(final String line) {
         this.line = line;
+        this.lineLength = line.length();
     }
 
+    /**
+     * Посчитаем длинну строки и количество
+     * открытых и закрытых скобок
+     *
+     * @return true если ОК
+     */
     public boolean isCorrect() {
-        throw new UnsupportedOperationException();
+        boolean result = true;
+        int opened = 0, closed = 0;
+        char[] charArray = this.line.toCharArray();
+
+        if (lineLength % 2 != 0) {
+            result = false;
+        }
+        if (result) {
+            for (int i = 0; i < this.lineLength; i++) {
+                if (charArray[i] == '(') opened++;
+                if (charArray[i] == ')') closed++;
+            }
+            if (opened != closed) result = false;
+        }
+        return result;
     }
 }
