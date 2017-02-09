f4838854740ef068e8ae38f2a0f6a486721283d8
Alex <kpecmuk@gmail.com>
2017-02-09 13:19:21 +0300

    09.02.17 13:19 Task 15 complete

--- src/main/java/ru/parsentev/task_015/CycleShift.java
+++ src/main/java/ru/parsentev/task_015/CycleShift.java
@@ -1,7 +1,6 @@
 package ru.parsentev.task_015;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -15,12 +14,36 @@ public class CycleShift {
     private static final Logger log = getLogger(CycleShift.class);
 
     private final int[] values;
+    private final int arraySize;
 
-    public CycleShift(final int[] values) {
+    /**
+     * Конструктор сохраняет массив и вычисляет его длинну
+     *
+     * @param values    массив
+     * @param arraySize длинна массива
+     */
+    public CycleShift(final int[] values, int arraySize) {
         this.values = values;
+        this.arraySize = values.length;
     }
 
+    /**
+     * Будем копировать массив, встроенными средствами
+     * arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+     * Шифтуем только если длинна массива позволяет
+     *
+     * @param position величина смещения
+     * @return массив int []
+     */
     public int[] shift(int position) {
-        throw new UnsupportedOperationException();
+        if (this.arraySize > 1) {
+            int tempValue;
+            for (int i = 0; i < position; i++) {
+                tempValue = this.values[this.arraySize - 1];
+                System.arraycopy(this.values, 0, this.values, 1, this.arraySize - 1);
+                this.values[0] = tempValue;
+            }
+        }
+        return this.values;
     }
 }
--- src/test/java/ru/parsentev/task_015/CycleShiftTest.java
+++ src/test/java/ru/parsentev/task_015/CycleShiftTest.java
@@ -1,8 +1,6 @@
 package ru.parsentev.task_015;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import ru.parsentev.task_014.ReversePhrase;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -13,19 +11,18 @@ import static org.junit.Assert.*;
  * @author parsentev
  * @since 28.07.2016
  */
-@Ignore
 public class CycleShiftTest {
 
     @Test
     public void multi() {
-        CycleShift cycle = new CycleShift(new int[] {1, 2, 3, 4, 5});
+        CycleShift cycle = new CycleShift(new int[]{1, 2, 3, 4, 5}, 0);
         int[] result = cycle.shift(2);
         assertThat(result, is(new int[] {4, 5, 1, 2, 3}));
     }
 
     @Test
     public void one() {
-        CycleShift cycle = new CycleShift(new int[] {1});
+        CycleShift cycle = new CycleShift(new int[]{1}, 0);
         int[] result = cycle.shift(2);
         assertThat(result, is(new int[] {1}));
     }
