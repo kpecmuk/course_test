bdfd40efeb87a3d7b85bcaa1878ce51611dc4566
Alex <kpecmuk@gmail.com>
2017-02-09 14:52:55 +0300

    09.02.17 14:55 Task 16 complete

--- src/main/java/ru/parsentev/task_016/TicTacToe.java
+++ src/main/java/ru/parsentev/task_016/TicTacToe.java
@@ -13,7 +13,6 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class TicTacToe {
     private static final Logger log = getLogger(TicTacToe.class);
-
     private final int[][] values;
 
     public TicTacToe(final int[][] values) {
@@ -21,6 +20,23 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        throw new UnsupportedOperationException();
+        boolean result = false;
+        for (int i = 0; i < this.values.length; i++) {
+            result = checkLines(i);
+            if (result) break;
+        }
+        return result;
+    }
+
+    private boolean checkLines(int line) {
+        boolean result = false;
+
+        if ((this.values[line][0] == this.values[line][1] && this.values[line][1] == this.values[line][2]) ||
+                (this.values[0][line] == this.values[1][line] && this.values[1][line] == this.values[2][line]) ||
+                (this.values[0][0] == this.values[1][1]) && (this.values[1][1] == this.values[2][2]) ||
+                (this.values[2][0] == this.values[1][1]) && (this.values[1][1] == this.values[0][2])) {
+            result = true;
+        }
+        return result;
     }
 }
